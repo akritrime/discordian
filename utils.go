136fe29d6f2808ef1b51a3fa00c4b8f25dd33a15
akritrime <akritrime@gmail.com>
2018-07-15 00:03:23 +0530

    trim the remaining content before returning

--- utils.go
+++ utils.go
@@ -8,14 +8,16 @@ import (
 
 // checks if a message is a command intended for the bot. Any message that starts with the guild's prefix or a bot mention is a command
 func isBotCommand(s, prefix string) (string, bool) {
+	trim := strings.TrimSpace
 	botMention := fmt.Sprintf("<@%v>", botID)
 	br := regexp.MustCompile(fmt.Sprintf("^%v", botMention))
+
 	if br.MatchString(s) {
-		return s[len(botMention):], true
+		return trim(s[len(botMention):]), true
 	}
 
 	if strings.HasPrefix(s, prefix) {
-		return s[len(prefix):], true
+		return trim(s[len(prefix):]), true
 	}
 
 	return "", false
